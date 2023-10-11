ebb8c9b4d9f32d3fdaf1c749ecb572eed7a2f861
mariazhestik <mariazhestik@gmail.com>
2023-10-11 23:19:37 +0100

    added cake.rb

--- cake.rb
+++ cake.rb
@@ -1,21 +1,21 @@
 # Функція для розрізання пирогу на прямокутні шматочки з родзинками.
 def slice_cake(cake, n)
-    raisin_count = cake.join.count('o') # Знаходимо загальну кількість родзинок в пирозі.
-    raisins_per_slice = raisin_count / n # Визначаємо, скільки родзинок має бути в кожному шматочку.
+    raisin_count = cake.join.count('o') 
+    raisins_per_slice = raisin_count / n 
   
-    slices = [] # Створюємо порожній список для зберігання шматочків.
-    slice = [] # Порожній список для поточного шматочка.
-    remaining_raisins = raisins_per_slice # Лічильник родзинок для поточного шматочка.
+    slices = [] 
+    slice = [] 
+    remaining_raisins = raisins_per_slice 
   
     # Проходимо через кожен рядок пирога.
     cake.each do |row|
-      slice << row # Додаємо рядок до поточного шматочка.
-      remaining_raisins -= row.count('o') # Віднімаємо кількість родзинок у рядку від лічильника.
+      slice << row 
+      remaining_raisins -= row.count('o') 
   
       if remaining_raisins == 0
-        slices << slice # Якщо лічильник дорівнює 0, додаємо поточний шматочок до списку.
-        slice = [] # Очищуємо поточний шматочок для наступного.
-        remaining_raisins = raisins_per_slice # Скидаємо лічильник родзинок для наступного шматочка.
+        slices << slice 
+        slice = [] 
+        remaining_raisins = raisins_per_slice 
       end
     end
   
